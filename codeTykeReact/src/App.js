f687e0d3fbcfe13da8a3eadceffe73eb4bd03384
PrestonManley <manley.preston@gmail.com>
2020-01-30 15:36:39 -0500

    refine css, add loader on initial load, add two more questions

--- codeTykeReact/src/App.js
+++ codeTykeReact/src/App.js
@@ -6,11 +6,14 @@ function App() {
   const [checkboxStatus, setCheckboxStatus] = React.useState([false, false, false, false]);
 
   const getQuestion=(questionId)=>{
-    fetch("http://localhost:8080/problems/"+questionId).then((res)=>{
-      return res.json();
-    }).then((data)=>{
-      setCurrentQuestion(data);
-    })
+    setTimeout(() =>{
+      fetch("http://localhost:8080/problems/"+questionId).then((res)=>{
+        return res.json();
+      }).then((data)=>{
+        setCurrentQuestion(data);
+      })
+    },
+    2500)
   }
 
   React.useEffect(()=>{
@@ -19,8 +22,10 @@ function App() {
 
   const handleSubmit=(event)=>{
     if(currentQuestion.id >= currentQuestion.totalQuestions){
+
       getQuestion(1);
     } else {
+      selectionElArr = (<div><p>Loading</p></div>);
       getQuestion(currentQuestion.nextQuestionId);
     }
   }
@@ -53,35 +58,37 @@ function App() {
       )
     })
   } else {
-    selectionElArr = [];
+    selectionElArr = (<div><p>Loading</p></div>);
   }
-  
-  var d = new Date();
-  var n = d.getFullYear();
 
   return (
-    <div id="mainWrapper">
-      <div id="headerBGColor"></div>
-      <div id="navbarContainer">
+    <div>
+    <div id="navbarContainer">
+      <div id="navbarContent">
         <img alt="logo" src="assets/logo.png" />
       </div>
-      <div id="questionHeaderContainer">
-        <div id="questionHeader">{currentQuestion.title}</div>
-        <div id="questionSubHeader">{currentQuestion.additionalInfo}</div>
-      </div>
-      <div id="outerBox">
-        {selectionElArr}
-        <div id="submitButtonContainer">
-          <div className={`submitButton ${checkboxStatus.includes(true) ? "active" : "disabled"}`} onClick={handleSubmit}>Submit</div>
+    </div>
+      <div id="mainWrapper">
+        <div id="questionHeaderContainer">
+          <div id="questionHeader">{currentQuestion.title}</div>
+          <div id="questionSubHeader">{currentQuestion.additionalInfo}</div>
+        </div>
+        <div id="outerBox">
+          {selectionElArr}
+          <div id="submitButtonContainer">
+            <div className={`submitButton ${checkboxStatus.includes(true) ? "active" : "disabled"}`} onClick={handleSubmit}>Submit</div>
+          </div>
         </div>
       </div>
       <div id="footerContainer">
-          <small id="copyright">Codetyke {n}</small>
+        <div id="footerContent">
+          <small id="copyright">&copy; Codetyke 2020</small>
           <small id="reserved">All rights reserved</small>
-          <small id="footerSlogan">We know you're ready to start coding</small>
+          <small id="footerSlogan">Get ready to start coding!</small>
+        </div>
       </div>
-      <div id="footerBGColor"></div>
     </div>
+
   );
 }
 
