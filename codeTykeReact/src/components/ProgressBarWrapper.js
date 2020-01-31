02223735a70e2ae1ecf40d64f845b5e51732d228
gjyhuang <grace.jy.huang@gmail.com>
2020-01-30 23:42:41 -0500

    cleared jura 2 (added progress bar)

--- codeTykeReact/src/components/ProgressBarWrapper.js
+++ codeTykeReact/src/components/ProgressBarWrapper.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import ProgressBar from './ProgressBar';
 
-// export default class ProgressBarWrapper extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       percentage: 0
-//     }
-//   }
-
-//   componentDidUpdate() {
-//     console.log('in hook:', this.props)
-//     const totalQuestions = this.props.currentQuestion.totalQuestions;
-//     const id = this.props.currentQuestion.id;
-//     const currPercentage = id/totalQuestions * 100;
-//     this.setState({percentage: currPercentage});
-//   }
-
-//   render() {
-//     console.log('in render:', this.props)
-//     // console.log(this.state.percentage)
-//     return <ProgressBar percentage={this.state.percentage}/>
-//   }
-// }
-
 const ProgressBarWrapper = props => {
   const [percentage, setPercentage] = React.useState(0);
 
