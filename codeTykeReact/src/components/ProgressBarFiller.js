828ef5cd1a285458be10abbce40d30ed5319faaf
gjyhuang <grace.jy.huang@gmail.com>
2020-02-08 18:06:09 -0500

    refactored out old unused files

--- codeTykeReact/src/components/ProgressBarFiller.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProgressBarFiller = (props) => {
-  return (
-    <div id="progressBarFiller" style={{width: `${props.percentage}%`}}/>
-  )
-}
-
-export default ProgressBarFiller;
--- codeTykeReact/src/components/ProgressBarFillerMax.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProgressBarFillerMax = (props) => {
-  return (
-    <div id="progressBarFiller" style={{width: `${props.percentage}%`, borderRadius: 20}}/>
-  )
-}
-
-export default ProgressBarFillerMax;
